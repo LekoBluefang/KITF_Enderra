16e378ce360659b3c5eaf015b585a4a56cab1cef
Purring Bytes <purringbytes@gmail.com>
2025-08-07 18:10:39 +0200

    Revert "Fixed Healing, removed forced cast of short (the one using (short)) a…"

--- src/client/include/MagixPlayer.h
+++ src/client/include/MagixPlayer.h
@@ -819,7 +819,7 @@ public:
 	{
 		if(invulnerable && ratio<0)return false;
 		hpChanged = true;
-		hp += ratio * maxHP;
+		hp += (short)ratio * maxHP;
 		if(hp>maxHP)hp = maxHP;
 		if(hp<0)hp = 0;
 		return (hp==0);
